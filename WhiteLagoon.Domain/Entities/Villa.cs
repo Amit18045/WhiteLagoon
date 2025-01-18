805308dc3b2b1ec9f7ce8cb540864a41c36686df
Amit singh Rawat <amitrawat1804@gmail.com>
2025-01-18 12:23:04 +0530

    Crud on villa section.

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -10,10 +10,13 @@ namespace WhiteLagoon.Domain.Entities
     public  class Villa
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public required string Name { get; set; }
         public string? Description { get; set; }
         [Display(Name="Price per night")]
+        [Range(10,10000)]
         public double Price { get; set; }
+        [Range(1,100)]
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
         [Display(Name="Image Url")]
--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers
@@ -19,6 +20,81 @@ namespace WhiteLagoon.Web.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa objVilla)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(objVilla);
+                _context.SaveChanges();
+                TempData["success"]="The Villa is created successfully.";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                TempData["error"]="The Villa Could not be created.";
+                return View(objVilla);
+            }
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _context.Villas.FirstOrDefault(u => u.Id==villaId);
+            if (obj == null)
+            {
+
+                return RedirectToAction("Error","Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa objVilla)
+        {
+            if (ModelState.IsValid && objVilla.Id>0)
+            {
+                _context.Update(objVilla);
+                _context.SaveChanges();
+                TempData["success"]="The Villa is updated successfully.";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                TempData["error"]="The Villa Could not be updated.";
+                return View(objVilla);
+            }
+        }
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _context.Villas.FirstOrDefault(u => u.Id==villaId);
+            if (obj == null)
+            {
+
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa objVilla)
+        {
+            Villa? objVillaFromDb = _context.Villas.FirstOrDefault(u => u.Id==objVilla.Id);
+            if (objVillaFromDb is not null)
+            {
+                _context.Remove(objVillaFromDb);
+                _context.SaveChanges();
+                TempData["success"]="The Villa is deleted successfully.";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                TempData["error"]="The Villa Could not be Deleted.";
+                return View(objVilla);
+            }
         }
     }
 }
